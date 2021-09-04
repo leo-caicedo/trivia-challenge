6c4e3bc3e277b31a076437d0be3c7fead1d786dd
Leonardo Caicedo <devleonardocaicedo@gmail.com>
2021-09-04 13:02:17 -0500

    Morgan implementation

--- src/app.js
+++ src/app.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const morgan = require('morgan')
 
 const createApp = () => {
   const app = express();
 
   // middleware
   app.use(express.json());
+  app.use(morgan('dev'));
 
   return app
 }
