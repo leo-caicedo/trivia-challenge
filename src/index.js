253f1d14df160f55562d58b98199687fd27183fa
Leonardo Caicedo <devleonardocaicedo@gmail.com>
2021-09-04 12:51:20 -0500

    Environment variables

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 const createApp = require('./app');
 const app = createApp();
+const { config } = require('./config')
 
-app.listen(3000, () => {
-  console.log('Server on port 3000');
+app.listen(config.port, (err) => {
+  if (err) return console.error(`ERROR ${err}`);
+  console.log(`Server on port ${config.port}`);
 });
