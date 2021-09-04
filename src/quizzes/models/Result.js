9d418bedb4fb0bcd795726b630ef11682203e438
Leonardo Caicedo <devleonardocaicedo@gmail.com>
2021-09-04 13:17:06 -0500

    Result model

---
+++ src/quizzes/models/Result.js
@@ -0,0 +1,29 @@
+const { Schema, model } = require('mongoose');
+
+const resultSchema = new Schema({
+    email: {
+        type: String,
+        required: true
+    },
+    quiz_id: {
+        type: Schema.Types.ObjectId,
+        ref: 'Quiz',
+    },
+    responses : [
+        {
+            question_id: {
+                type: Schema.Types.ObjectId,
+                ref: 'Question',
+	        required: true
+            },
+            my_response: {
+                type: Boolean,
+                required: true
+            }
+        },
+    ]
+}, {
+    versionKey: false
+});
+
+module.exports = model('Result', resultSchema);
