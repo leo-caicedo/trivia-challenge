96d2df67389d7175badbf2112a0899eb51666583
Leonardo Caicedo <devleonardocaicedo@gmail.com>
2021-09-04 20:32:42 -0500

    unique name validation in quiestions

--- src/quizzes/dtos/quiz.dto.js
+++ src/quizzes/dtos/quiz.dto.js
@@ -1,12 +1,20 @@
 const { body } = require('express-validator');
 
+// models
+const Quiz = require('../models/Quiz.js')
+
 const quizDto = [
   // quiz name
   body('name')
     .exists({ checkFalsy: false })
     .withMessage('Quiz name is required')
     .isString()
-    .withMessage('Quiz name must be a string'),
+    .withMessage('Quiz name must be a string')
+  .custom((name) => {
+    return Quiz.findOne({ name }).then((quiz) => {
+      if (quiz) return Promise.reject('Quiz name already in use');
+    });
+  }),
 ]
 
 module.exports = quizDto;
