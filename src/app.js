12ea88fb664b387407881baa339a0f6643cd57a4
Leonardo Caicedo <devleonardocaicedo@gmail.com>
2021-09-04 23:43:10 -0500

    home

--- src/app.js
+++ src/app.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
+const hbs = require('express-handlebars');
 
+const path = require('path');
 // required routes
 const questionsRoutes = require('./quizzes/routes/questions.routes.js');
 const quizzesRoutes = require('./quizzes/routes/quizzes.routes.js');
@@ -15,10 +17,31 @@ const createApp = () => {
   app.use(express.json());
   app.use(morgan('dev'));
 
+  // settings
+  app.set("views", path.join(__dirname, "views"));
+  app.engine(
+    ".hbs",
+    hbs({
+      defaultLayout: "main",
+      extname: ".hbs",
+    })
+  );
+  app.set("view engine", ".hbs");
+
   // routes
   app.use('/api/questions', questionsRoutes);
   app.use('/api/quizzes', quizzesRoutes);
   app.use('/api/results', resultsRoutes);
+
+  // home page
+  app.get('/home', (req, res) => {
+    res.render('home');
+  })
+
+  // redirect
+  app.get('/', (req, res) => {
+    res.redirect('/home');
+  });
 
   return app
 }
